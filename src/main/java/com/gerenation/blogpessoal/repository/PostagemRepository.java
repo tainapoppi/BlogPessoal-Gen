4877bdaa27487995af1a98f612e828cb6d8396de
Taina Poppi <tainapoppi@hotmail.com>
2024-03-15 14:08:11 -0300

    Blog Pessoal completo

---
+++ src/main/java/com/gerenation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.gerenation.blogpessoal.repository;
+	
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.gerenation.blogpessoal.model.Postagem;
+
+
+public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param ("titulo") String titulo);
+
+}
